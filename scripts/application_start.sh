7c1efc6691163408475ea044a25c2c7e797bf419
Nik Weinmeister <nik.weinmeister@excella.com>
2019-02-18 14:36:16 -0500

    set secret key base

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd /home/ubuntu/travis-demo
-sudo export SECRET_KEY_BASE="$(rake secret)"
-sudo kill $(cat /opt/nginx/logs/nginx.pid)
+export SECRET_KEY_BASE="$(rake secret)"
+kill $(cat /opt/nginx/logs/nginx.pid)
 /opt/nginx/sbin/nginx
