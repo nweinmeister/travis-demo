2f891787af239ad60d7d423ff4ca2f7414b2cf1d
Nik Weinmeister <nik.weinmeister@excella.com>
2019-02-18 12:57:39 -0500

    Point to files on server

--- after_install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /home/ubuntu/travis-demo
-bundle install
-bundle exec rake db:migrate RAILS_ENV=production
--- application_start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo kill $(cat /opt/nginx/logs/nginx.pid)
-/opt/nginx/sbin/nginx
